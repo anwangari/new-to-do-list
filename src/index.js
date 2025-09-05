c7f05216007b4adb582ce576f2e4737d050a4108
Anthony Njuguna <8034679+anwangari@users.noreply.github.com>
2025-09-05 16:27:54 +0300

    modified:   src/index.js modified:   src/pages/main.js
    Initialized the building of dashboard container.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import "./styles.css";
+import { renderDashboard } from "./pages/main.js";
 
 console.log("Javascript Working Correctly.");
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,1 +1,5 @@
-import { createElement } from '../utils/domHelpers';
+import { createElement, clearElement, appendChildren } from '../../utils/dom-helpers.js';
+
+export function renderDashboard() {
+    const dashContainer = createElement('div', {className: 'dash-container'})
+}
